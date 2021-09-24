42219640590bbf542c5c1431d2323ef534c21216
zengsz01 <2546262970@qq.com>
2021-09-24 10:58:08 +0800

    改写了find_gamma.py
    在原有程序的基础上，针对接收到峰的质量不同进行分类处理。在接收到一个及以上高质量峰时处理方式与
    之前相同；当未接收到高质量峰时，应用新的处理方法。
    
    zengsz01

--- find_gamma.py
+++ find_gamma.py
@@ -2,13 +2,11 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import denoise as dn
-import coordinate_system as coor
 import response_matrix as res
 from tqdm import tqdm
-
-with h5py.File('data/deposition.h5', 'r') as source:
-    ebounds = np.array(source['Matrix'].attrs['Edges'])
-    index = np.array(source['Matrix'].attrs['Index'])
+import gamma_detector as gd
+import cube_system as cs
+import pandas as pd
 
 # 将球坐标转化为直角坐标
 def transformer(spherical):
@@ -31,198 +29,223 @@ def inverse_transformer(cartesian):
 # 矢量归一化
 def renormalize(array):
     norm = np.sqrt(np.sum(array ** 2))
-    return array/norm
-
-
-################################################
-# 以下部分处理真实输入
-################################################
-
-Cube_list = ['D0', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6']
-energy_total_real = []
-
-for cube_i in range(7):
-    with h5py.File('data/train_1.h5', 'r') as source:
-        response_E_T = source['1']['response_E_T'][Cube_list[cube_i]]['0'][:]
-
-    # 读取起始时间和结束时间
-    time_inf = int(np.ceil(response_E_T[0][0]))
-    time_sup = int(np.floor(response_E_T[-1][0]))
-
-    total_time = time_sup - time_inf
-    time_interval = 0.1    # 设定的时间间隔
-    time_num = int(total_time/time_interval)
-
-    Spectrum = np.zeros(time_num)
-
-    # 对于每一个粒子，按时间和能量（只考虑第0能道）归类
-    for k in tqdm(range(response_E_T.shape[0])):
-        # 对时间归类
-        for i in range(time_num):
-            if response_E_T[k][0] > time_inf + time_interval*i and response_E_T[k][0] < time_inf + time_interval*(i + 1):
-                if response_E_T[k][1] > ebounds[0] and response_E_T[k][1] < ebounds[1]:
-                    Spectrum[i] += response_E_T[k][1]
-                break 
-                # 对能量归类
-                '''
-                for j in range(100):
-                    if response_E_T[k][1] > ebounds[j] and response_E_T[k][1] < ebounds[j+1]:
-                        Spectrum[i][j] += response_E_T[k][1]
-                        break
-                break
-                '''
-
-    # 降噪，去平均
-    Spectrum_denoise = dn.denoise_curve_by_fft(Spectrum, method='number', freq_num=50)
-    energy_aver = np.sum(Spectrum_denoise)/time_num
-    Spectrum_denoise -= energy_aver
-
-    ###########################
-    # 寻找峰和峰的起始，结束位置
-    ###########################
-
-    # 寻找最值点
-    energy_max = Spectrum_denoise.max()
-    max_location = np.argmax(Spectrum_denoise)
-    energy_min = Spectrum_denoise.min()
-
-    # 判断是否存在峰
-    if energy_max + 2 * energy_min > 0:
-
-        # 寻找峰起始和结束的位置
-        gamma_range = []
-
-        # 从最大位置向前寻找零点
-        i = max_location
-        while True:
-            if Spectrum_denoise[i-1] < 0 and Spectrum_denoise[i+1] > 0:
-                gamma_range.append(i)
-                break
-            i -= 1
-
-        # 从最大位置向后寻找零点
-        i = max_location
-        while True:
-            if Spectrum_denoise[i+1] < 0 and Spectrum_denoise[i-1] > 0:
-                gamma_range.append(i)
-                break
-            i += 1
-        
-        # 计算Gamma爆总能量（使用了原始能谱并减去了噪声）
-        noise_aver = (gamma_range[1] - gamma_range[0])*np.sum(Spectrum[:gamma_range[0]])/gamma_range[0]
-        energy_total_real.append([np.sum(Spectrum[gamma_range[0]:gamma_range[1]]) - noise_aver, cube_i])
-
-energy_total_real = np.array(energy_total_real)
-
-
-   
-################################################
-# 以下部分在不同角度模拟输入
-################################################
-
-# 读取输入能道
-in_ebounds = []
-for i in index[::84]:
-    in_ebounds.append(i[0])
-in_ebounds = np.array(in_ebounds)
-
-# 生成模拟的输入（伽马源）能谱
-input_energy = []
-for i in range(40):
-    if i == 0:
-        input_energy.append((in_ebounds[1] - in_ebounds[0])/(in_ebounds[i] ** 2))
-    elif i == 39:
-        input_energy.append((in_ebounds[39] - in_ebounds[38])/(in_ebounds[i] ** 2))
+    if norm != 0.0:
+        return array/norm
     else:
-        input_energy.append((in_ebounds[i + 1] - in_ebounds[i-1])/(2 * in_ebounds[i] ** 2))
-input_energy = np.array(input_energy)
+        return array
+
+# 以输入矢量为中心生成模拟入射的位置
+def generate(guess_spherical):
+
+    # 生成以初始矢量为z轴的900个位置的球坐标
+    theta_list = np.linspace(0, np.pi/2, 15)
+    phi_list = np.linspace(0, 2*np.pi, 60)
+    theta, phi = np.meshgrid(theta_list, phi_list)
+    guess_points = np.stack((theta, phi), axis=2)
+
+    # 生成以初始矢量为z轴的900个位置的笛卡尔坐标
+    guess_points_cartesian = np.empty([60, 15, 3])
+    for i in range(60):
+        for j in range(15):
+            guess_points_cartesian[i][j] = transformer(guess_points[i][j])
+
+    # 将以初始矢量为z轴的笛卡尔坐标转化为地球系中的笛卡尔坐标
+    transform_matrix = np.matmul([[np.cos(guess_spherical[1]), -np.sin(guess_spherical[1]), 0],
+                                [np.sin(guess_spherical[1]), np.cos(guess_spherical[1]), 0],
+                                [0, 0, 1]], 
+                                [[np.cos(guess_spherical[0]), 0, np.sin(guess_spherical[0])],
+                                [0, 1, 0],
+                                [-np.sin(guess_spherical[0]), 0, np.cos(guess_spherical[0])]])
+
+    transform_matrix = transform_matrix.T
+
+    for i in range(60):
+        for j in range(15):
+            guess_points_cartesian[i][j] = np.matmul(guess_points_cartesian[i][j], transform_matrix)
+
+    return guess_points_cartesian
+
+# 将球坐标转为GCRS
+def spherical_to_gcrs(spherical):
+    ra = spherical[1]
+    dec = np.pi / 2 - spherical[0]
+    return np.array([ra, dec])
+
+# 生成模拟输入的能谱
+E_edges = pd.read_csv('data/Eedges.csv', header=None).to_numpy().squeeze()
+Eng_in = (1 / E_edges - np.roll(1 / E_edges, -1))[:-1]
+
+# 初始化总误差,误差列表和输出
+# error = 0
+# error_list = np.empty(500)
+source = []
+
+test_list = ['test_1.h5', 'test_2.h5']
+for test_i in range(2):
+
+    # 对500个事件循环
+    for event_num in tqdm(range(500)):
+
+        file_path = 'data/{}'.format(test_list[test_i])
+        event = '{}'.format(event_num + 1 + 500*test_i)
+
+        # 读入真实信号源方位
+        '''
+        with h5py.File('data/{}'.format(list[test_i]), 'r') as source:
+            real_source = np.array(source['{}'.format(event_num+1+500*test_i)]['source'])
+
+        real_source_spherical = np.empty(2)
+        real_source_spherical[0] = np.pi/2 - real_source[0][1]
+        real_source_spherical[1] = real_source[0][0]
+        real_source_cartesian = transformer(real_source_spherical)
+        '''
+
+        # 初始化
+        Gamma = gd.gamma_event(file_path, event)
+
+        Cubes = cs.cube_system(file_path, event)
+
+        ################################################
+        # 以下部分处理真实输入
+        ################################################
+        
+        # 对每个卫星的四块晶体取平均
+        gamma_energy_real = np.sum(Gamma.Eng_out_gamma_all, axis=1)
+
+        # 判断每次事件有几颗卫星接收到高质量峰
+        rate_limit = 3.5
+        gamma_rate = np.sum(Gamma.Eng_rate, axis=1)/4
+        cube_count = np.count_nonzero(gamma_rate > rate_limit)
+
+        ################################################
+        # 以下部分根据接收到高质量峰的卫星数的不同进行不同的模拟
+        ################################################
+
+        #########################
+        # 没有卫星接收到可靠的信号
+        #########################
+        if cube_count == 0:
+
+            # 生成模拟入射的位置
+            theta_list = np.linspace(0, np.pi, 30)
+            phi_list = np.linspace(0, 2*np.pi, 60)
+            theta, phi = np.meshgrid(theta_list, phi_list)
+            guess_points = np.stack((theta, phi), axis=2)
+
+            guess_points_cartesian = np.empty([60, 30, 3])
+            for i in range(60):
+                for j in range(30):
+                    guess_points_cartesian[i][j] = transformer(guess_points[i][j])
+
+            # 对每个位置模拟输入并计算评分
+            score = np.empty([60, 30])
+
+            # 评分方案一：响应矩阵元之和
+            '''
+            for i in range(60):
+                for j in range(30):                
+                    res_matrix = Cubes.get_response(guess_points_cartesian[i][j])
+
+                    score[i][j] = res_matrix.sum()
+            '''
+
+            # 评分方案二： 模拟的各卫星的第零能道能量平方和
+            for i in range(60):
+                for j in range(30):
+                    
+                    res_matrix = Cubes.get_response(guess_points_cartesian[i][j])
+
+                    gamma_energy_theoretical = []
+                    
+                    for k in range(7):
+                        theoretical_spectrum = np.zeros(100)
+                        for l in range(4):
+                            theoretical_spectrum += np.matmul(Eng_in, res_matrix[k][l])
+                        gamma_energy_theoretical.append(theoretical_spectrum[0:3])
+
+                    gamma_energy_theoretical = np.array(gamma_energy_theoretical)
+
+                    # alpha和beta为第1，2能道的权重                    
+                    alpha = 0
+                    beta = 0
+                    
+                    score[i][j] = np.sum(gamma_energy_theoretical[:, 0] ** 2) \
+                                + alpha * np.sum(gamma_energy_theoretical[:, 1] ** 2) \
+                                + beta * np.sum(gamma_energy_theoretical[:, 2] ** 2)
+
+            # 将评分最高的位置作为输出
+            min_location_temp = np.argmin(score)
+            result_cartesian = guess_points_cartesian[int(min_location_temp // 30)][int(min_location_temp % 30)]
 
 
-# 初始化并读取各卫星朝向
-coor.event_init('1', 'data/train_1.h5')
-cube_normal = coor.Cube_coor_frame_z
+        ###########################
+        # 有一颗卫星接收到可靠的信号
+        ###########################
+           
+        ###############################
+        # 有一颗以上卫星接收到可靠的信号
+        ###############################
+        else:
 
-# 将接收到信号的卫星的法向矢量相加（并归一）作为初始（猜测）矢量
-guess_cartesian = np.zeros(3)
-for i in energy_total_real[:, 1]:
-    guess_cartesian += cube_normal[int(i)]
-guess_spherical = inverse_transformer(renormalize(guess_cartesian))
+            # 处理真实能谱
+            gamma_energy_real = renormalize(gamma_energy_real[:, 0:3])
+            # 读取各卫星朝向
+            cube_normal = Cubes.Cube_coor_frame_z
 
-################################
-# 生成900个位置的笛卡尔坐标
-################################
+            # 将接收到高质量峰的卫星的法向矢量相加（并归一）作为初始（猜测）矢量
+            guess_cartesian = np.zeros(3)
+            for i in range(7):
+                if (gamma_rate > rate_limit)[i]:
+                    guess_cartesian += cube_normal[i]
+            guess_spherical = inverse_transformer(renormalize(guess_cartesian))
 
-# 生成以初始矢量为z轴的900个位置的球坐标
-theta_list = np.linspace(0, np.pi/2, 15)
-phi_list = np.linspace(0, 2*np.pi, 60)
-theta, phi = np.meshgrid(theta_list, phi_list)
-guess_points = np.stack((theta, phi), axis=2)
+            # 在以初始矢量为中心的半球上均匀生成模拟的入射点
+            guess_points_cartesian = generate(guess_spherical)
+            
+            # 对每个位置模拟输入并计算评分
+            score = np.empty([60, 15])
 
-# 生成以初始矢量为z轴的900个位置的笛卡尔坐标
-guess_points_cartesian = np.empty([60, 15, 3])
-for i in range(60):
-    for j in range(15):
-        guess_points_cartesian[i][j] = transformer(guess_points[i][j])
+            for i in range(60):
+                for j in range(15):
+                    
+                    res_matrix = Cubes.get_response(guess_points_cartesian[i][j])
 
-# 将以初始矢量为z轴的笛卡尔坐标转化为地球系中的笛卡尔坐标
-transform_matrix = np.matmul([[np.cos(guess_spherical[1]), -np.sin(guess_spherical[1]), 0],
-                              [np.sin(guess_spherical[1]), np.cos(guess_spherical[1]), 0],
-                              [0, 0, 1]], 
-                              [[np.cos(guess_spherical[0]), 0, np.sin(guess_spherical[0])],
-                               [0, 1, 0],
-                               [-np.sin(guess_spherical[0]), 0, np.cos(guess_spherical[0])]])
+                    gamma_energy_theoretical = []
+                    
+                    for k in range(7):
+                        theoretical_spectrum = np.zeros(100)
+                        for l in range(4):
+                            theoretical_spectrum += np.matmul(Eng_in, res_matrix[k][l])
+                        gamma_energy_theoretical.append(theoretical_spectrum[0:3])
 
-transform_matrix = transform_matrix.T
+                    gamma_energy_theoretical = renormalize(np.array(gamma_energy_theoretical))
+                    
+                    alpha = 0
+                    beta = 0
+                    
+                    score[i][j] = np.sum((gamma_energy_real[:, 0] - gamma_energy_theoretical[:, 0]) ** 2) \
+                                + alpha * np.sum((gamma_energy_real[:, 1] - gamma_energy_theoretical[:, 1]) ** 2) \
+                                + beta * np.sum((gamma_energy_real[:, 2] - gamma_energy_theoretical[:, 2]) ** 2)
+                    
+            # 将评分最高的位置作为输出
+            min_location_temp = np.argmin(score)
+            result_cartesian = guess_points_cartesian[int(min_location_temp // 15)][int(min_location_temp % 15)]
+            #print(int(min_location_temp // 15), int(min_location_temp % 15))
+        
 
-for i in range(60):
-    for j in range(15):
-        guess_points_cartesian[i][j] = np.matmul(guess_points_cartesian[i][j], transform_matrix)
+        #error_list[event_num] = np.arccos(np.dot(result_cartesian, real_source_cartesian))
+        #error += np.arccos(np.dot(result_cartesian, real_source_cartesian))
+        
+        result_spherical = inverse_transformer(result_cartesian)
+        source.append(spherical_to_gcrs(result_spherical))
+    '''
+    with h5py.File('error_list{}.h5'.format(test_i), 'w') as output:
+        output['error'] = error_list
 
-# 对每个位置模拟输入并计算评分（当前评分是理论与实际能量比的方差）
-var_result = np.empty([60, 15])
+    print(error)
+    '''
 
-for i in tqdm(range(60)):
-    for j in range(15):
-        cube_theta, cube_phi = coor.get_angular_coor_in_cubes(guess_points_cartesian[i][j])
-        res_matrix = res.get_Response_Matrix(cube_theta, cube_phi, method='array')
-
-        energy_total_theoretical = []
-        for k in energy_total_real[:, 1]:
-            theoretical_spectrum = np.matmul(input_energy, res_matrix[int(k)][0])
-            energy_total_theoretical.append(theoretical_spectrum[0])
-
-        energy_total_theoretical = np.array(energy_total_theoretical)
-        energy_total_theoretical = renormalize(energy_total_theoretical)
-        beta = energy_total_theoretical/energy_total_real[:, 0]
-
-        var_result[i][j] = np.var(beta)
-
-# 将评分最高的位置作为输出（此处输出的是theta, phi）
-min_location_temp = np.argmin(var_result)
-print(inverse_transformer(guess_points_cartesian[int(min_location_temp // 15)][int(min_location_temp % 15)]))
-
-# 保存各位置的评分以方便查看
-with h5py.File('var_result.h5', 'w') as output:
-    output['var_result'] = var_result
-
-
-# 以下为画图功能
-'''
-time_axis = np.zeros(time_num)
-for i in range(time_num):
-    time_axis[i] = time_inf + time_interval*i
-
-#plt.scatter(gamma_location[0], gamma_location[1])
-#plt.plot(time_axis, Spectrum_denoise, 'black')
-plt.plot(time_axis, Spectrum[:, 0], 'red')
-plt.plot(time_axis, dn.denoise_curve_by_gauss(Spectrum[:, 0], 2), 'black')
-plt.plot(time_axis, dn.denoise_curve_by_fft(Spectrum[:, 0], method='number', freq_num=50), 'lime')
-plt.show()
+with h5py.File('source.h5', 'w') as output:
+    output['source'] = np.array(source)
 
 
-with h5py.File('Spectrum.h5', 'w') as output:
-    output['Spectrum'] = Spectrum
-
-plt.plot(response_E_T[5700:6700,0], np.log10(response_E_T[5700:6700,1]), 'o', ms=1)
-plt.savefig('test1_x.png')
-'''
